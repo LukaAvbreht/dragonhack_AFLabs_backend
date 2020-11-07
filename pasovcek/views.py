488e37e3d20d6aadc440d480430ad56b60d5f0a5
Avbreht <ziga.avbreht@gmail.com>
2020-11-07 19:48:02 +0100

    add filters for nesreca

--- pasovcek/views.py
+++ pasovcek/views.py
@@ -13,7 +13,23 @@ class NesrecaViewSet(viewsets.GenericViewSet,
     serializer_class = NesrecaSerializer
 
     def get_queryset(self):
-        return super().get_queryset()
+        nesrece = Nesreca.objects.all()
+
+        datum_gte = self.request.query_params.get("datum_gte", None)
+        if datum_gte is not None:
+            nesrece = nesrece.filter(datum__gte=datum_gte)
+        datum_lte = self.request.query_params.get("datum_lte", None)
+        if datum_lte is not None:
+            nesrece = nesrece.filter(datum__lte=datum_lte)
+
+        filters = ["lokacija","vrsta_ceste","sifra_ceste","text_ceste_naselja","sifra_odseka_ulice",
+                   "text_odseka_ulice","stacionaza_dogodka","opis_kraja","vzrok_nesrece","tip_nesrece",
+                   "vremenske_okoliscine","stanje_prometa","stanje_vozisca","vrsta_vozisca"]
+        for filter in filters:
+            if self.request.query_params.get(filter, None) is not None:
+                nesrece = nesrece.filter(**{filter: self.request.query_params.get(filter, None)})
+
+        return nesrece
     
     @action(methods=["GET"], detail=False)
     def letna_statistika(self, request, *args, **kwargs):
@@ -21,7 +37,7 @@ class NesrecaViewSet(viewsets.GenericViewSet,
 
     @action(methods=["get"], detail=False)
     def mesecna_statistika(self, request, *args, **kwargs):
-        return Reponse("Tu bo prišla mesečna statistika")
+        return Response("Tu bo prišla mesečna statistika")
 
 class OsebaViewSet(viewsets.GenericViewSet,
                 mixins.RetrieveModelMixin,
