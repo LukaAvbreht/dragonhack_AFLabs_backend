6cc5b01637c3928f918118d78efe833b0750e797
Avbreht <ziga.avbreht@gmail.com>
2020-11-07 20:57:41 +0100

    update admin

--- pasovcek/admin.py
+++ pasovcek/admin.py
@@ -65,12 +65,40 @@ class VrstaUdelezencaAdmin(admin.ModelAdmin):
 class DrzavljanstvoAdmin(admin.ModelAdmin):
     list_display = ("pk", "ime")
 
+@admin.register(TextCesteNaselja)
+class TextCesteNaseljaAdmin(admin.ModelAdmin):
+    list_display = ("pk", "ime")
+    search_fields = ["pk"]
+
+@admin.register(TextOdsekaUlice)
+class TextOdsekaUliceAdmin(admin.ModelAdmin):
+    list_display = ("pk", "ime")
+    search_fields = ["pk"]
+
 
 @admin.register(Oseba)
 class OsebaAdmin(admin.ModelAdmin):
-    list_display = ("nesreca", "je_povzrocitelj", "starost", "spol", "poskodba", "vrsta_udelezenca",
-    "vozniski_staz", "vrednost_alkotesta", "drzavljanstvo", "ue_prebivalisca")
+    list_display = ("vrsta_udelezenca", "je_povzrocitelj", "starost", "spol",
+                    # "poskodba", "vozniski_staz",
+                    "vrednost_alkotesta", "drzavljanstvo", "ue_prebivalisca",
+                    "nesreca__text_ceste_naselja__ime",
+                    # "nesreca__text_odseka_ulice__ime",
+                    "nesreca__vzrok_nesrece__ime", "nesreca__tip_nesrece__ime")
     list_filter = ("spol", "je_povzrocitelj", "drzavljanstvo", "poskodba", "vrsta_udelezenca", "ue_prebivalisca")
+    autocomplete_fields = ["nesreca"]
+
+    def nesreca__text_ceste_naselja__ime(self, obj):
+        return obj.nesreca.text_ceste_naselja.ime
+
+    def nesreca__text_odseka_ulice__ime(self, obj):
+        return obj.nesreca.text_odseka_ulice.ime
+
+    def nesreca__vzrok_nesrece__ime(self, obj):
+        return obj.nesreca.vzrok_nesrece.ime
+
+    def nesreca__tip_nesrece__ime(self, obj):
+        return obj.nesreca.tip_nesrece.ime
+
 
 @admin.register(Nesreca)
 class NesrecaAdmin(admin.ModelAdmin):
@@ -78,3 +106,5 @@ class NesrecaAdmin(admin.ModelAdmin):
     "vzrok_nesrece", "tip_nesrece")
 
     list_filter = ("ura", "klasifikacija", "vrsta_ceste", "vzrok_nesrece", "tip_nesrece")
+    search_fields = ["pk"]
+    autocomplete_fields = ["text_ceste_naselja", "text_odseka_ulice"]
